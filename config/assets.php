dad46ae4a0797b827ae31086e724ef39a145af71
Nikolay <Nikolayd2002@gmail.com>
2025-06-21 15:12:33 +0200

    Vitejs config

---
+++ config/assets.php
@@ -0,0 +1,10 @@
+<?php
+
+    return [
+        'css' => [
+            'main' => 'resources/assets/css/main.css',
+        ],
+        'js' => [
+            'main' => 'resources/assets/js/main.js',
+        ],
+    ];
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+
+                // В будущем можно подключать компоненты
+                // 'resources/components/Button/button.css',
+                // 'resources/components/Button/button.js',
+            ],
             refresh: true,
         }),
     ],
