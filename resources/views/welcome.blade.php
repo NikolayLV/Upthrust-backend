f80179bde7353d4beb4d6e76112b8e1089e2ba11
Nikolay <Nikolayd2002@gmail.com>
2025-06-21 18:28:30 +0200

    Updated loading animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,16 +13,35 @@
     </head>
     <body class="antialiased">
     <div class="loading_container">
+        <!-- From Uiverse.io by Admin12121 -->
         <div class="loader">
-            <div class="square" id="sq1"></div>
-            <div class="square" id="sq2"></div>
-            <div class="square" id="sq3"></div>
-            <div class="square" id="sq4"></div>
-            <div class="square" id="sq5"></div>
-            <div class="square" id="sq6"></div>
-            <div class="square" id="sq7"></div>
-            <div class="square" id="sq8"></div>
-            <div class="square" id="sq9"></div>
+            <div class="box box0">
+                <div></div>
+            </div>
+            <div class="box box1">
+                <div></div>
+            </div>
+            <div class="box box2">
+                <div></div>
+            </div>
+            <div class="box box3">
+                <div></div>
+            </div>
+            <div class="box box4">
+                <div></div>
+            </div>
+            <div class="box box5">
+                <div></div>
+            </div>
+            <div class="box box6">
+                <div></div>
+            </div>
+            <div class="box box7">
+                <div></div>
+            </div>
+            <div class="ground">
+                <div></div>
+            </div>
         </div>
     </div>
     </body>
